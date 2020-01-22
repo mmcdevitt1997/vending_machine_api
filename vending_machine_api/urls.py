af5064dd9b1372b8f8c1e3b6a817637648150327
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-01-22 16:15:27 -0600

    switching branchs

--- vending_machine_api/urls.py
+++ vending_machine_api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
-
+from vendingapp.views improt *
 
 router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'inventory', Inventory)
+
 
 urlpatterns = [
     url(r'^', include(router.urls)),
